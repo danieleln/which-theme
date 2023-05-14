821b521fc46f70393d57f2d1a6b482a86919002a
danieleln <daniele.monzani@outlook.com>
2023-05-14 15:14:39 +0200

    Added lua/which-theme.lua, plugin/which-theme.lua

---
+++ plugin/which-theme.lua
@@ -0,0 +1,17 @@
+-- prevents loading which-theme twice
+if vim.g.loaded_which_theme then
+    return
+end
+
+
+-- ex-commands
+vim.api.nvim_add_user_command(
+    'WhichTheme',
+    function()
+
+    end,
+    { nargs = 0 }
+)
+
+
+vim.g.loaded_which_theme = true
