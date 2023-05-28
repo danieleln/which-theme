cb125965b3d1c22b4ba0b27d40ffeeb3feb4b48d
danieleln <daniele.monzani@outlook.com>
2023-05-28 07:15:02 +0200

    add: basic lua module stucture

---
+++ lua/which-theme/init.lua
@@ -0,0 +1,9 @@
+local options = require('which-theme.options')
+local themes  = require('which-theme.themes')
+local window  = require('which-theme.window')
+local utils   = require('which-theme.utils')
+
+local M = {}
+
+
+return M
